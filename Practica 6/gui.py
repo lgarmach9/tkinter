c479508ac0c2c368c474083dd5e5ce84f11ca078
lgarmach9 <LGARCIAM814@alumnos.imf.com>
2024-01-17 16:46:28 +0100

    Practica 6. interfaces

--- Practica 6/gui.py
+++ Practica 6/gui.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import tkinter as tk
+from tkinter import ttk
 
 raiz = tk.Tk()
 
@@ -10,10 +11,14 @@ for campo in xml.find_all("campo"):
     tipo = campo.get("tipo")
     texto = campo.get("texto")
     if tipo == "entrada":
-        tk.Entry(raiz).pack(padx=20,pady=20)
+        ttk.Entry(raiz).pack(padx=20,pady=20)
     elif tipo== "etiqueta":
-        tk.Label(raiz, text=texto).pack(padx=20,pady=20)
+        ttk.Label(raiz, text=texto).pack(padx=20,pady=20)
     elif tipo == "boton":
-        tk.Button(raiz, text=texto).pack(padx=20,pady=20)
+        ttk.Button(raiz, text=texto).pack(padx=20,pady=20)
+    elif tipo == "deslizable":
+        ttk.Scale(raiz, from_=0, to=100).pack(padx=50,pady=50)
+    elif tipo == "seleccion":
+        ttk.Radiobutton(raiz, text=texto).pack(padx=50,pady=50)
 
 tk.mainloop()
